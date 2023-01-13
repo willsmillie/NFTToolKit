ae1f78a0eeb0e3e41d348c3e14b85fb45dfc11d4
Will Smillie <will@smillie.me>
2023-01-13 10:46:47 -0500

    [tryReadFile] add json array parsing

--- src/utils/tryReadFile.js
+++ src/utils/tryReadFile.js
@@ -2,11 +2,13 @@ const fs = require("fs");
 const path = require("path");
 
 function tryReadFile(pathItem) {
+  pathItem = pathItem.trim();
   let exists = !!path.extname(pathItem);
   if (exists) {
     try {
       return fs.readFileSync(pathItem, "utf8");
     } catch (error) {
+      console.error(error);
       return pathItem;
     }
   } else {
